7c287a409a8b1e571a5575fd20b8b053c8d705c9
Lego Chiang <qas612820704@gmail.com>
2018-05-04 03:16:23 +0800

    Add Header and icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { injectGlobal } from 'styled-components';
+import Header from './components/Header';
+import logo from './logo.png';
 import './App.css';
+
+/* eslint-disable no-unused-expressions */
+injectGlobal`
+  a {
+    color: #1464e8;
+  }
+`
+/* eslint-enable no-unused-expressions */
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
