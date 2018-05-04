eca4dc3a4530ad039ecec3160d73cd449ae2a3ba
Lego Chiang <qas612820704@gmail.com>
2018-05-04 15:43:16 +0800

    Add fetch data from github issues

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+fetch('https://api.github.com/repos/TGmeetup/TGmeetup.github.io/issues?labels=Event&state=open')
+  .then(res => res.json())
+  .then(issues => issues.map(issue => {
+    const reDetailText = /<details>((?:.|[\r\n])*?)<\/detail>/gm;
+    const { body } = issue;
+
+    const eventStr = reDetailText.exec(body)[1];
+    const event = JSON.parse(eventStr);
+
+    return {
+      ...event,
+      issue,
+    };
+  }));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
